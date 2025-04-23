982a8853e95aaf45c9518dcc001969e042632b5e
crow117coca <rearstar01@gmail.com>
2025-04-23 21:10:36 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+name="Arnav"
+age=10
+waight=28.4
+blooms="true"
+
+print("value of name ",name)
+print("the data type name",type (name))
+print("value of name ",age)
+print("the data type name",type (age))
+print("value of name ",waight)
+print("the data type name",type (waight))
+print("value name ",blooms)
+print("the data type name",type (blooms))
