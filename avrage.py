dd211470a3c75507321458ae0af6ff8937cb947b
crow117coca <rearstar01@gmail.com>
2025-04-30 21:18:37 +0100

    Add files via upload

---
+++ avrage.py
@@ -0,0 +1,12 @@
+a = 23
+b = 43
+c = 32
+
+if a > 23 or b > 23:
+    print("the a is true one")
+else:
+    print("this is false one")
+if a > 23 and b > 43:
+    print("all of it is true")
+else:
+    print("all of it is false")
