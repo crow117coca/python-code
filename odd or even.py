e5287c00d4fe300f4f62adbae4216f1945d9dad6
crow117coca <rearstar01@gmail.com>
2025-05-16 21:29:37 +0100

    Add files via upload

---
+++ odd or even.py
@@ -0,0 +1,11 @@
+print("pick a odd number")
+a=31
+b=99
+c=32
+choice=input("chose carfuly:a=99 b=31 c=32")
+if choice==("a"):
+    print("corrcet")
+elif choice==("b"):
+    print("your right!")
+elif choice==("c"):
+    print("i told to choose carfuly")
---
+++ sum.py
@@ -0,0 +1,5 @@
+print("pick a nuber")
+a=12
+b=23
+choice=input("heres the nuber: a=12 b=23")
+print("thank you")
