264ba6c429303edf571344fa883d1a6eb51e2a5a
crow117coca <rearstar01@gmail.com>
2025-05-21 20:20:20 +0100

    Add files via upload

---
+++ class.py
@@ -0,0 +1,18 @@
+print("4 people graduated choose your stoudents")
+print("hi i am student A i am 19 years old")
+print("hi i am student B i am 21 years old")
+print("hello i am student C i am 18 years old")
+print("yo i am student D i am 20 years old")
+A=19
+B=21
+C=18
+D=20
+choice=input("choose your student:A=19 B=21 C=18 B=20")
+if choice==("A"):
+    print("your to young")
+elif choice==("b"):
+    print("your old but fine")
+elif choice==("c"):
+    print("your to young")
+elif choice==("D"):
+    print("your perfect")
